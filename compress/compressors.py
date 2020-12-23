bed62622c37c028090adbb72236f4e1110a78a63
Janosch <janosch.ruff@stud.uni-goettingen.de>
2020-12-23 19:44:31 +0100

    Adding a version with plain BWT

--- compress/compressors.py
+++ compress/compressors.py
@@ -14,12 +14,24 @@ def bijective_huffman_compress(omega):
     binary = huf.huffman_compress(string_mtf, code_book)
     return(binary, code_book, alphabet)
 
-string = "ananasibananamamissisipi"
-bin, note, alph = bijective_huffman_compress(string)
-
 #Decompression for algorithm based on Bijective BWT, MTF and Huffman encoding
 def bijective_huffman_decompress(binary, notebook, alphabet):
     string_mtf = huf.huffman_decompress(binary, notebook)
     string_bwt = mtf.MTF_decode(string_mtf, alphabet)
     omega = bwt.BBWT_inv(string_bwt)
+    return(omega)
+
+#Compression based on plain BWT, MTF and Huffman encoding
+def bwt_huffman_compress(omega):
+    string_bwt = bwt.BWT(omega)
+    string_mtf, alphabet = mtf.MTF_encode(string_bwt)
+    code_book = huf.huffman_coding(string_mtf)
+    binary = huf.huffman_compress(string_mtf, code_book)
+    return(binary, code_book, alphabet)
+
+#Decompression for algorithm based on Bijective BWT, MTF and Huffman encoding
+def bwt_huffman_decompress(binary, notebook, alphabet):
+    string_mtf = huf.huffman_decompress(binary, notebook)
+    string_bwt = mtf.MTF_decode(string_mtf, alphabet)
+    omega = bwt.BWT_inv(string_bwt)
     return(omega)
