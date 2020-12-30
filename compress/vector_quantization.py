11741c8e8e8f79ee7fd28663e92766f38548f4d4
makavelj <janosch.ruff@stud.uni-goettingen.de>
2020-12-30 03:55:33 +0100

    Create vector_quantization.py
    Adding simple K-Means implementation for Vector Quantization

---
+++ compress/vector_quantization.py
@@ -0,0 +1,55 @@
+"""
+Implementation of Vector Quantization based on simple K-Means Clustering.
+"""
+
+import numpy as np
+import random 
+
+
+MAX = 9999999999999999
+
+#Vector Quantization algorithm for clustering
+def vector_quantization(X, vectors=5, epsilon=1E-4, max_iteration=100, distance=euclidean):
+  X = np.array(X)
+  n = len(X[0])
+  m = len(X)
+  min, max = [], []
+  #Setup 1: Get min and max values for all attributes
+  for i in range(n):
+    min.append(np.amin(X[:,i]))
+    max.append(np.amax(X[:,i]))
+  means = []
+  #Setup 2: Initialize random starting points based on random samples
+  for i in range(vectors):
+    mean = []
+    for j in range(n):
+      mean.append(random.uniform(min[j], max[j]))
+    means.append(mean)
+  clusters = [0]*m
+  iterations = 0
+  eps = MAX
+  #Start main process of clustering points
+  while(iterations < max_iteration and eps > epsilon):
+    #Step 1: Cluster points to closest vector
+    for i in range(m):
+      min = MAX
+      for j in range(vectors):
+        dist = distance(X[i],means[j])
+        if(dist < min):
+          min = dist
+          clusters[i] = j
+    #Step 2: Optimize distance of vector to clustered points
+    eps = 0
+    for i in range(vectors):
+      epsilon_diff = 0
+      indices = [j for j, x in enumerate(clusters) if x == i]
+      sum = [0]*n
+      for j in range(len(indices)):
+        sum += X[indices[j]]
+      if(len(indices) > 0):
+        means_old = means[i]
+        means[i] = [x / len(indices) for x in sum]
+        epsilon_diff = distance(means[i], means_old)
+      eps += epsilon_diff
+    iterations += 1
+  return clusters, means
