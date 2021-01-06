e123106e153d0b2ee80e64afea5152f1b399ff37
makavelj <janosch.ruff@stud.uni-goettingen.de>
2021-01-06 12:16:34 +0100

    Include BWT Testing in main

--- compress/test_bwt.py
+++ compress/test_bwt.py
@@ -1,12 +1,14 @@
 '''
-Tests for the Burrows Wheeler Transform
+Testing of the Burrows Wheeler Transform
 '''
 
 import bwt
 
-
-string =  "ananasibananamamissisipi"
-
 #check if string does not change after transform and inversion
-assert(bwt.BWT_inv(bwt.BWT(string)) == string)
-assert(bwt.BBWT_inv(bwt.BBWT(string)) == string)
+def BWT_test():
+    string =  "ananasibananamamissisipi"
+    assert(bwt.BWT_inv(bwt.BWT(string)) == string), ('Plain BWT does not recover original string')
+    assert(bwt.BBWT_inv(bwt.BBWT(string)) == string), ('Bijective BWT does not recover original string')
+
+if __name__ == '__main__':
+    BWT_test()
